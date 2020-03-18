7206f1a95edb6d2a44b6740ebc335d36b9828648
James Veugelaers <james.veugelaers@outlook.co.nz>
2020-03-18 23:12:17 +1300

    First pass

---
+++ Assets/Scenes/SampleScene.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleScene : MonoBehaviour
+{
+    public enum SampleEnum
+    {
+        Option0 = 0,
+        Option1 = 1,
+        Option2 = 2,
+        Option3 = 3,
+        Option4 = 4,
+    }
+    
+    public InputField StringInputField;
+    public Toggle BoolToggle;
+    public Dropdown EnumDropdown;
+    public InputField IntInputField;
+    public InputField FloatInputField;
+    public InputField DoubleInputField;
+    
+    // List
+    public Text listTextSize;
+    public GameObject listTextTemplate;
+    public GameObject listContainer;
+    
+    // Array
+    public Text arrayTextSize;
+    public GameObject arrayTextTemplate;
+    public GameObject arrayContainer;
+
+    private string stringValue;
+    private bool boolValue;
+    private SampleEnum enumValue;
+    private int intValue;
+    private float floatValue;
+    private double doubleValue;
+    private List<string> listOfStrings;
+    private string[] arrayOfStrings;
+
+    private void Start()
+    {
+        string[] options = Enum.GetNames(typeof(SampleEnum));
+        EnumDropdown.ClearOptions();
+        EnumDropdown.AddOptions(new List<string>(options));
+        
+        listTextTemplate.SetActive(false);
+        arrayTextTemplate.SetActive(false);
+    }
+
+    public void Load()
+    {
+        stringValue = Storage.instance.Load("string_key", "nothing previously saved");
+        boolValue = Storage.instance.Load("bool_key", false);
+        enumValue = Storage.instance.Load("enum_key", SampleEnum.Option0);
+        intValue = Storage.instance.Load("int_key", -1);
+        floatValue = Storage.instance.Load("float_key", -1.0f);
+        doubleValue = Storage.instance.Load("double_key", -1.0);
+        listOfStrings = Storage.instance.LoadList("stringList_key", new List<string>());
+        arrayOfStrings = Storage.instance.LoadArray("stringArray_key", new string[0]);
+
+        StringInputField.text = stringValue;
+        BoolToggle.isOn = boolValue;
+        EnumDropdown.value = (int)enumValue;
+        IntInputField.text = intValue.ToString();
+        FloatInputField.text = floatValue.ToString();
+        DoubleInputField.text = doubleValue.ToString();
+        PopulateList();
+        PopulateArray();
+    }
+    
+#region List UI
+    private void PopulateList()
+    {
+        for (int i = 1; i < listContainer.transform.childCount; i++)
+        {
+            Destroy(listContainer.transform.GetChild(i).gameObject);
+        }
+
+        listTextSize.text = listOfStrings == null ? "null" : listOfStrings.Count.ToString();
+        if (listOfStrings != null)
+        {
+            for (int i = 0; i < listOfStrings.Count; i++)
+            {
+                AddItemToList(i);
+            }
+        }
+    }
+
+    public void AddNewItemToList()
+    {
+        listOfStrings.Add("item" + listOfStrings.Count);
+        AddItemToList(listOfStrings.Count - 1);
+    }
+    
+    public void AddItemToList(int index)
+    {
+        GameObject button = Instantiate(listTextTemplate, listContainer.transform);
+        button.transform.GetChild(0).GetComponent<Text>().text = listOfStrings[index] == null ? "null" : listOfStrings[index];
+        button.GetComponent<Button>().onClick.AddListener(delegate
+        {
+            int j = index; 
+            RemoveItemFromList(j); 
+        });
+        button.gameObject.SetActive(true);
+        listTextSize.text = listOfStrings == null ? "null" : listOfStrings.Count.ToString();
+    }
+    
+    public void RemoveItemFromList(int index)
+    {
+        listOfStrings.RemoveAt(index);
+        PopulateList();
+    }
+#endregion
+    
+#region Array UI
+    public void IncreaseArraySize()
+    {
+        string[] copy = new string[arrayOfStrings.Length + 1];
+        for (int i = 0; i < arrayOfStrings.Length; i++)
+        {
+            copy[i] = arrayOfStrings[i];
+        }
+
+        arrayOfStrings = copy;
+        PopulateArray();
+    }
+    
+    public void DecreaseArraySize()
+    {
+        string[] copy = new string[Mathf.Max(0,arrayOfStrings.Length - 1)];
+        for (int i = 0; i < copy.Length; i++)
+        {
+            copy[i] = arrayOfStrings[i];
+        }
+
+        arrayOfStrings = copy;
+        PopulateArray();
+    }
+
+    public void PressedArrayItem(GameObject button)
+    {
+        int childIndex = button.transform.GetSiblingIndex() - 1;
+        arrayOfStrings[childIndex] = arrayOfStrings[childIndex] == null ? "item" + childIndex : null;
+        PopulateArray();
+    }
+
+    private void PopulateArray()
+    {
+        for (int i = 1; i < arrayContainer.transform.childCount; i++)
+        {
+            Destroy(arrayContainer.transform.GetChild(i).gameObject);
+        }
+
+        arrayTextSize.text = arrayOfStrings == null ? "null" : arrayOfStrings.Length.ToString();
+        if (arrayOfStrings != null)
+        {
+            for (int i = 0; i < arrayOfStrings.Length; i++)
+            {
+                GameObject button = Instantiate(arrayTextTemplate, arrayContainer.transform);
+                button.transform.GetChild(0).GetComponent<Text>().text = arrayOfStrings[i] == null ? "null" : arrayOfStrings[i];
+                button.GetComponent<Button>().onClick.AddListener(delegate
+                {
+                    PressedArrayItem(button); 
+                });
+                button.gameObject.SetActive(true);
+                listTextSize.text = arrayOfStrings == null ? "null" : arrayOfStrings.Length.ToString();
+            }
+        }
+    }
+#endregion
+
+    public void Save()
+    {
+        Storage.instance.Save("string_key", StringInputField.text);
+        Storage.instance.Save("bool_key", BoolToggle.isOn);
+        Storage.instance.Save("enum_key", (SampleEnum)EnumDropdown.value);
+        Storage.instance.Save("int_key", int.Parse(IntInputField.text));
+        Storage.instance.Save("float_key", float.Parse(FloatInputField.text));
+        Storage.instance.Save("double_key", double.Parse(DoubleInputField.text));
+        Storage.instance.SaveList("stringList_key", listOfStrings);
+        Storage.instance.SaveArray("stringArray_key", arrayOfStrings);
+    }
+    
+    public void Clear()
+    {
+        StringInputField.text = "";
+        BoolToggle.isOn = false;
+        EnumDropdown.value = 0;
+        IntInputField.text = "";
+        FloatInputField.text = "";
+        DoubleInputField.text = "";
+        listOfStrings = null;
+        PopulateList();
+        arrayOfStrings = null;
+        PopulateArray();
+    }
+    
+    public void DeletePlayerPrefs()
+    {
+        Storage.instance.DeleteAll();
+    }
+}
---
+++ Assets/Storage.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storage
+{
+    public interface ISaveable
+    {
+        void Save();
+    }
+    
+    public static Storage instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = new Storage();
+            }
+
+            return m_instance;
+        }
+    }
+    private static Storage m_instance;
+
+    private List<ISaveable> m_queue = new List<ISaveable>();
+
+    private void Update()
+    {
+        if (m_queue.Count == 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < m_queue.Count; i++)
+        {
+            m_queue[i].Save();
+        }
+
+        m_queue.Clear();
+        PlayerPrefs.Save();
+    }
+
+    public void QueueSave(ISaveable saveable)
+    {
+        m_queue.Add(saveable);
+    }
+
+    public List<T> LoadList<T>(string key, List<T> defaultValue = null)
+    {
+        int size = Load(key + "_count", -1);
+        if (size < 0)
+        {
+            // Does not exist in PlayerPrefs
+            return defaultValue;
+        }
+        
+        List<T> list = new List<T>(size);
+        for (int i = 0; i < size; i++)
+        {
+            T t = Load(key + "_" + i, default(T));
+            list.Add(t);
+        }
+
+        return list;
+    }
+    
+    public T[] LoadArray<T>(string key, T[] defaultValue = null)
+    {
+        int size = Load(key + "_count", -1);
+        if (size < 0)
+        {
+            // Does not exist in PlayerPrefs
+            return defaultValue;
+        }
+        
+        T[] array = new T[size];
+        for (int i = 0; i < size; i++)
+        {
+            T t = Load(key + "_" + i, default(T));
+            array[i] = t;
+        }
+
+        return array;
+    }
+    
+    public T Load<T>(string key, T defaultValue)
+    {
+        if (!PlayerPrefs.HasKey(key))
+            return defaultValue;
+
+        if (typeof(T) == typeof(String))
+        {
+            string s = PlayerPrefs.GetString(key);
+            return (T)(object)(s != "-null" ? s : null);
+        }
+        else if (typeof(T) == typeof(float))
+        {
+            return (T)(object)PlayerPrefs.GetFloat(key);
+        }
+        else if (typeof(T) == typeof(int))
+        {
+            return (T)(object)PlayerPrefs.GetInt(key);
+        }
+        else if (typeof(T) == typeof(double))
+        {
+            string s = PlayerPrefs.GetString(key);
+            return (T)(object)double.Parse(s);
+        }
+        else if (typeof(T) == typeof(bool))
+        {
+            int i = PlayerPrefs.GetInt(key);
+            return (T)(object)(i == 1);
+        }
+        else if (typeof(T).IsEnum)
+        {
+            return (T)(object)PlayerPrefs.GetInt(key);
+        }
+
+
+        throw new Exception("Unknown Type " + typeof(T));
+    }
+    
+    private void Save(string key, object value)
+    {
+        if (value == null)
+        {
+            Save(key, "-null");
+        }
+        else if (value is System.String s1)
+        {
+            Save(key, s1);
+        }
+        else if (value is float f1)
+        {
+            Save(key, f1);
+        }
+        else if (value is int i1)
+        {
+            Save(key, i1);
+        }
+        else if (value is double d1)
+        {
+            Save(key, d1);
+        }
+        else if (value is bool b1)
+        {
+            Save(key, b1);
+        }
+        else if (value.GetType().IsEnum)
+        {
+            Save(key, Convert.ToUInt64(value));
+        }
+        else
+        {
+            throw new Exception("Unknown Type " + value.GetType());
+        }
+    }
+
+    public bool KeyExists(string key)
+    {
+        return PlayerPrefs.HasKey(key);
+    }
+    
+    public void Save(string key, string value)
+    {
+        PlayerPrefs.SetString(key, value);
+    }
+
+    public void Save(string key, int value)
+    {
+        PlayerPrefs.SetInt(key, value);
+    }
+
+    public void Save(string key, float value)
+    {
+        PlayerPrefs.SetFloat(key, value);
+    }
+
+    public void Save(string key, double value)
+    {
+        PlayerPrefs.SetString(key, value.ToString("R"));
+    }
+
+    public void Save(string key, bool value)
+    {
+        PlayerPrefs.SetInt(key, value ? 1 : 0);
+    }
+
+    public void Save(string key, Enum value)
+    {
+        PlayerPrefs.SetInt(key, Convert.ToInt32(value));
+    }
+    
+    public void SaveList<T>(string key, List<T> list)
+    {
+        if (list == null)
+        {
+            return;
+        }
+        
+        // Save size
+        int size = list.Count;
+        Save(key + "_count", size);
+        
+        // Save each item
+        for (int i = 0; i < size; i++)
+        {
+            Save(key + "_" + i, list[i]);
+        }
+    }
+    
+    public void SaveArray<T>(string key, T[] list)
+    {
+        if (list == null)
+        {
+            return;
+        }
+        
+        // Save size
+        int size = list.Length;
+        Save(key + "_count", size);
+        
+        // Save each item
+        for (int i = 0; i < size; i++)
+        {
+            Save(key + "_" + i, list[i]);
+        }
+    }
+    
+    public void DeleteAll()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
